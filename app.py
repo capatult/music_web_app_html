0920d52a0a978462ee7572e66f77ddd75346fad1
Perran Thomas <perran.thomas@icloud.com>
2023-10-11 16:10:39 +0100

    Put basic GET routes into app.py (with their corresponding tests in tests/test_app.py

--- app.py
+++ app.py
@@ -1,12 +1,28 @@
 import os
 from flask import Flask, request, render_template
 from lib.database_connection import get_flask_database_connection
+from lib.album_repository import AlbumRepository
+from lib.album import Album
+from lib.artist_repository import ArtistRepository
+from lib.artist import Artist
 
 # Create a new Flask app
 app = Flask(__name__)
 
 # == Your Routes Here ==
+@app.route('/albums', methods=['GET'])
+def get_albums():
+    connection = get_flask_database_connection(app)
+    repository = AlbumRepository(connection)
+    return str(
+        repository.all()
+    )
 
+@app.route('/artists', methods=['GET'])
+def get_artists():
+    connection = get_flask_database_connection(app)
+    repository = ArtistRepository(connection)
+    return str(repository.all())
 
 
 # These lines start the server if you run this file directly
